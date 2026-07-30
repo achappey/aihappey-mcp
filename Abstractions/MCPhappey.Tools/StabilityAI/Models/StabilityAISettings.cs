0573a1a066ff613f8deb99c462e6473d8de1aaa3
Arthur Bleij <github@achappey.nl>
2026-07-30 18:46:57 +0200

    Remove deprecated Stability AI abstractions and tool services

--- Abstractions/MCPhappey.Tools/StabilityAI/Models/StabilityAISettings.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-
-namespace MCPhappey.Tools.StabilityAI.Models;
-
-public class StabilityAISettings
-{
-    [Description("Your Stability AI API key.")]
-    public string ApiKey { get; set; } = default!;
-}
